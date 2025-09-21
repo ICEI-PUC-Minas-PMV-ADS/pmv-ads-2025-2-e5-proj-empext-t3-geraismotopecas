bdd38fa0b9e4bdaa90d744f9b2e7bf4114f66e6d
Sara Guanais <guanaissara@gmail.com>
2025-09-20 21:40:41 -0300

    criar novo serviço

---
+++ codigo-fonte/backend_api_node/src/routes/servicoRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const Servico = require("../models/servico")
+
+
+
+// CRIAR UM NOVO SERVIÇO
+
+router.post("/", async (req, res) => {
+    try {
+        const novoServico = new Servico(req.body);
+        await novoServico.save();
+        res.status(201).json(novoServico);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
+

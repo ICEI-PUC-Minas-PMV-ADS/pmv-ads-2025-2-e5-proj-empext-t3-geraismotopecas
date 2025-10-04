24269e545ea19df7f61d3641e6ca97363cdb8688
Mia Nascimento <mianascimento.dev@gmail.com>
2025-10-04 13:20:28 -0300

    separa Sidebar da Home

--- codigo-fonte/frontend_app_react/web/src/components/Sidebar.js
+++ codigo-fonte/frontend_app_react/web/src/components/Sidebar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/Home.css";
+import "../styles/Sidebar.css";
 import logoImage from "../images/logolivro.png";
 
 function Sidebar({ children }) {
